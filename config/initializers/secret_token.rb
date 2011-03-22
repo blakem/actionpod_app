0bd9c41d7b88675c17e7a044b2aaee29e4550c97
Blake Mills <blakem30@yahoo.com>
2011-03-21 17:23:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActionpodApp::Application.config.secret_token = 'b1c6aea51f896d10e23296ff6b7645b4e1f9872d0999daefa272633f29c1b12be5088afd5ab1c1a43e5f0375a2427db4665f2896958d1a3c8af36921840cab86'
