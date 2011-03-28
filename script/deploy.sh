a8c257b35fc81f764300f9dd5f3926d20b5585e1
Blake Mills <blakem30@yahoo.com>
2011-03-28 12:55:11 -0700

    Added heroku rake db:migrate

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,8 @@ git push
 
 echo; echo "************ git push heroku"
 git push heroku
+echo; echo "************ heroku rake db:migrate"
+heroku rake db:migrate
 echo "Heroku Done....."
 
 script/migrate_all_databases.sh
