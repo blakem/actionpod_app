b107d576a536fc9663fb583a70ab4c346bfd39e1
Blake Mills <blakem30@yahoo.com>
2011-03-28 15:58:57 -0700

    added test for admin_signed_in?

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,7 @@ describe PagesController do
   render_views
   describe "when not logged in" do
     it "should redirect to the login page" do
-	  controller.user_signed_in?.should be_false
+  	  controller.user_signed_in?.should be_false
       get 'home'
       response.should redirect_to(:action=>"new", :controller=>"devise/sessions")
     end
@@ -14,10 +14,22 @@ describe PagesController do
     login_user
  
     it "should be successful when logged in" do
-	  controller.user_signed_in?.should be_true
+	    controller.user_signed_in?.should be_true
       get 'home'
       response.should be_success
       response.should have_selector('title', :content => 'ActionPods')
     end   
+
+    it "should be successful when logged in as admin" do
+	    controller.user_signed_in?.should be_true
+      controller.admin_signed_in?.should be_false
+      controller.current_user.admin = "true"
+      controller.current_user.save
+      controller.admin_signed_in?.should be_true      
+      get 'home'
+      response.should be_success
+      response.should have_selector('title', :content => 'ActionPods')
+    end   
+
   end
 end
