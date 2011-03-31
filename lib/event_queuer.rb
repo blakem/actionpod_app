5d7bd2fadf71b77807608bdb73b7ebcab3bcaf2a
Blake Mills <blakem30@yahoo.com>
2011-03-31 16:45:43 -0700

    Fixed wrongly checked in eventqueuer

--- lib/event_queuer.rb
+++ lib/event_queuer.rb
@@ -2,7 +2,7 @@ class EventQueuer
   def queue_events(time = Time.now)
     Event.all.each do |event|
       if event.schedule.occurs_on?(time)
-        event.make_call
+        event.delay.make_call
       end
     end
   end
