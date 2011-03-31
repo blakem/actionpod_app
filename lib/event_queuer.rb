c53f04c26ab15dcb6a4671f339cbc35d59a05bc7
Blake Mills <blakem30@yahoo.com>
2011-03-31 16:51:05 -0700

    minor change for live test

--- lib/event_queuer.rb
+++ lib/event_queuer.rb
@@ -2,7 +2,7 @@ class EventQueuer
   def queue_events(time = Time.now)
     Event.all.each do |event|
       if event.schedule.occurs_on?(time)
-        event.delay.make_call
+        event.make_call
       end
     end
   end
