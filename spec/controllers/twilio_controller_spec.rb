ed82186827b4e836846783c6d8bd57c3e831fff6
Blake Mills <blakem30@yahoo.com>
2011-04-01 11:57:27 -0700

    Say Hello

--- spec/controllers/twilio_controller_spec.rb
+++ spec/controllers/twilio_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe TwilioController do
+  render_views
   describe "when not logged in" do
     it "should be success" do
   	  controller.user_signed_in?.should be_false
       get 'greeting.xml'
+      response.content_type.should =~ /^application\/xml/
       response.should be_success
+    end
+  end
+
+  describe "greeting" do
+    it "should say hello" do
+      get 'greeting.xml'
+      response.content_type.should =~ /^application\/xml/
+      response.should have_selector('response>say', :content => 'Hello')
     end
   end
 end
