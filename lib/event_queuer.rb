2e586470c017329b79fb4ade8fbc1f44b2f305a2
Blake Mills <blakem30@yahoo.com>
2011-04-03 22:57:04 -0700

    EventQueuer returns number of calls queued

--- lib/event_queuer.rb
+++ lib/event_queuer.rb
@@ -1,11 +1,14 @@
 class EventQueuer
   def queue_events(time = Time.now.utc)
+    count = 0
     Event.all.each do |event|
       schedule = event.schedule
       next_run_time = schedule.first.utc
       if next_run_time < Time.now.tomorrow.utc
         event.delay(:run_at => next_run_time).make_call
+        count += 1
       end
     end
+    count
   end
 end
