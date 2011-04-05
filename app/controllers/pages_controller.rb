d6f161dd62d13870017db6f07b2f0e389a0522cd
Blake Mills <blakem30@yahoo.com>
2011-04-05 13:15:27 -0700

    Collect occurrences in Callcal by current_user.time_zone

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,8 +12,11 @@ class PagesController < ApplicationController
   end
   
   def callcal
-    redirect_to(root_path) unless user_signed_in?
-    @scheduled_events = build_scheduled_events
+    if user_signed_in?
+      @scheduled_events = build_scheduled_events
+    else
+      redirect_to(root_path) unless user_signed_in?
+    end
   end
   
   private
@@ -23,6 +26,7 @@ class PagesController < ApplicationController
       end_time = start_time + 7.days
       Event.all.each do |event|
         event.schedule.occurrences_between(start_time, end_time).each do |occurrence|
+          occurrence = occurrence.in_time_zone(current_user.time_zone)
           hash[occurrence] ||= 0
           hash[occurrence] += 1
         end
