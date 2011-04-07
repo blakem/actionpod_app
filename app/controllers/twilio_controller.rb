468e58a0e8d060d30752cf3fd7b396cd9087011d
Blake Mills <blakem30@yahoo.com>
2011-04-07 12:34:23 -0700

    Templatize postto and event_name

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -5,14 +5,11 @@ class TwilioController < ApplicationController
     event = find_event_from_params(params)
     unless event
       self.say_sorry
-      output = render_to_string :action => :say_sorry
+      render :action => :say_sorry
     else
       @event_name = event.name
       @postto = base_url + '/put_on_hold.xml'
-      output  = render_to_string
     end
-    $stderr.puts output if Rails.env.production?
-    render :inline => output, :format => :xml
   end
   
   def say_sorry
