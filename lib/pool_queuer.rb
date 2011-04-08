84466af9e886e8d9e7afc1b10eb395e5e052888c
Blake Mills <blakem30@yahoo.com>
2011-04-07 17:29:06 -0700

    Syntax error fix

--- lib/pool_queuer.rb
+++ lib/pool_queuer.rb
@@ -22,24 +22,24 @@ class PoolQueuer
   end
     
   def check_before_calls_go_out(pool, pool_runs_at)
-    jobs = DelayedJob.where(
-      :run_at => pool_runs_at,
-      :pool_id => pool.id,
-      :obj_type    => 'Event', 
-      :obj_jobtype => 'make_call',
-    )
-    if jobs.count == 1
-      job = jobs.first
-      event = Event.find_by_id(job.obj_id)
-      if (event)
-        TwilioCaller.new.send_sms(
-          event.user.primary_phone, 
-          "Sorry.  No one else is scheduled for the #{event.time} slot.  This shouldn't happen after we reach a critical mass of users. ;-)"
-        )
-      end
-      job.destroy
-    else 
-      # Schedule the next piece
-    end
+    # jobs = DelayedJob.where(
+    #   :run_at => pool_runs_at,
+    #   :pool_id => pool.id,
+    #   :obj_type    => 'Event', 
+    #   :obj_jobtype => 'make_call',
+    # )
+    # if jobs.count == 1
+    #   job = jobs.first
+    #   event = Event.find_by_id(job.obj_id)
+    #   if (event)
+    #     TwilioCaller.new.send_sms(
+    #       event.user.primary_phone, 
+    #       "Sorry.  No one else is scheduled for the #{event.time} slot.  This shouldn't happen after we reach a critical mass of users. ;-)"
+    #     )
+    #   end
+    #   job.destroy
+    # else 
+    #   # Schedule the next piece
+    # end
   end
 end
