269140b5c4b24741c9e21e70ff2ecff64d54f05e
Blake Mills <blakem30@yahoo.com>
2011-04-08 16:09:31 -0700

    Intermediate checkin point for new merging algorithm

--- lib/pool_merger.rb
+++ lib/pool_merger.rb
@@ -1,9 +1,49 @@
 class PoolMerger
 
+  # Get list of new participants
+  # Split them into groups of 3 using old participants first
+    # Put groups of three into a conference room
+  
+  # Three Cases:
+    # 0 left - You're done
+    # 1 left
+       # leave him there if there is no existing conference
+       # leave him there if he's new
+       # put him in the smallest conference room if he's old
+    # 2 left
+       # if either is old unite them
+       # if they are both new leave them there
+
   def merge_calls_for_pool(pool, data)
     @tc = TwilioCaller.new
-    @tc.participants_on_hold_for_pool(pool).each do |participant|
-      @tc.place_participant_in_conference(participant[:call_sid], "Pool#{pool.id}Room1", pool.timelimit)
+    data = initialize_data(data)
+    new_participants = @tc.participants_on_hold_for_pool(pool)
+    return data if new_participants.empty?
+    room_name = next_room(pool, data)
+    new_participants.each do |participant|
+      @tc.place_participant_in_conference(participant[:call_sid], room_name, pool.timelimit)
+      data[:conferences][0][:members] += 1
+    end
+    data
+  end
+
+  def next_room(pool, data)
+    room_number = data[:next_room]
+    data[:next_room] += 1
+    conference = "Pool#{pool.id}Room#{room_number}"
+    data[:conferences] << { :name => conference, :members => 0 }
+    conference
+  end
+
+  def initialize_data(data)
+    if data.empty?
+      return {
+        :next_room => 1,
+        :conferences => [],
+        :on_hold => [],
+      }
+    else
+      return  data
     end
   end
 
--- spec/lib/pool_merger_spec.rb
+++ spec/lib/pool_merger_spec.rb
@@ -3,22 +3,53 @@ require 'spec_helper'
 describe PoolMerger do
   before(:each) do
     @pm = PoolMerger.new
+    @pool = Factory(:pool)
+    @tc = mock('TwilioCaller')
+    TwilioCaller.stub(:new).and_return(@tc)
   end
 
   describe "merge_calls_for_pool" do
-    it "sends each participant into a room" do
-      tc = mock('TwilioCaller')
-      TwilioCaller.should_receive(:new).and_return(tc)
-      pool = Factory(:pool, :timelimit => 22)
-      tc.should_receive(:participants_on_hold_for_pool).with(pool).and_return(participant_hash)
-      tc.should_receive(:place_participant_in_conference).with("CA9fa67e8696b60ee1ca1e75ec81ef85e7", "Pool#{pool.id}Room1", 22)
-      @pm.merge_calls_for_pool(pool, {})
+    it "does nothing with zero new participants" do
+      @tc.should_receive(:participants_on_hold_for_pool).with(@pool).and_return(participant_list(0))
+      @pm.merge_calls_for_pool(@pool, {}).should == {
+        :next_room   => 1,
+        :conferences => [], 
+        :on_hold     => [],
+      }
+    end
+    
+    it "should unite three new participants" do
+      new_participants = participant_list(3)
+      @tc.should_receive(:participants_on_hold_for_pool).with(@pool).and_return(new_participants)
+      @tc.should_receive(:place_participant_in_conference).with("CA9fa67e8696b60ee1ca1e75ec81ef85e7XXX1", "Pool#{@pool.id}Room1", @pool.timelimit)
+      @tc.should_receive(:place_participant_in_conference).with("CA9fa67e8696b60ee1ca1e75ec81ef85e7XXX2", "Pool#{@pool.id}Room1", @pool.timelimit)
+      @tc.should_receive(:place_participant_in_conference).with("CA9fa67e8696b60ee1ca1e75ec81ef85e7XXX3", "Pool#{@pool.id}Room1", @pool.timelimit)
+      @pm.merge_calls_for_pool(@pool, {}).should == {
+        :next_room   => 2,
+        :conferences => [{:name => "Pool#{@pool.id}Room1", :members => 3}], 
+        :on_hold     => [],
+      }
+    end
+  end
+
+  describe "initialize_data" do
+    it "should initialize the empty hash" do
+      @pm.initialize_data({}).should == {
+        :next_room => 1,
+        :conferences => [],
+        :on_hold => [],
+      }
+    end
+
+    it "should leave existing values alone" do
+      data = {:next_room => 1, :conferences => [1,2,3], :on_hold => [4,5,6]}
+      @pm.initialize_data(data).should == data
     end
   end
 
 end
 
-def participant_response
+def one_participant_response
   '{"page":0,"num_pages":1,"page_size":50,"total":1,"start":0,"end":0,"uri":"\/2010-04-01\/Accounts\/AC2e57bf710b77d765d280786bc07dbacc\/Conferences\/' +
   'CF0cb07a25bdaf64828850b784ea2d1aa7\/Participants.json","first_page_uri":"\/2010-04-01\/Accounts\/AC2e57bf710b77d765d280786bc07dbacc\/Conferences\/' +
   'CF0cb07a25bdaf64828850b784ea2d1aa7\/Participants.json?Page=0&PageSize=50","previous_page_uri":null,"next_page_uri":null,"last_page_uri":"\/2010-04-01\/' +
@@ -29,6 +60,17 @@ def participant_response
   'AC2e57bf710b77d765d280786bc07dbacc\/Conferences\/CF0cb07a25bdaf64828850b784ea2d1aa7\/Participants\/CA9fa67e8696b60ee1ca1e75ec81ef85e7.json"}]}'
 end
 
-def participant_hash
-  ActiveSupport::JSON.decode(participant_response).with_indifferent_access[:participants]
+def participant_list(participant_count)
+  list = []
+  stub_participant = ActiveSupport::JSON.decode(one_participant_response).with_indifferent_access[:participants][0]
+  (1..participant_count).each do |i|
+    participant = stub_participant.dup
+    participant[:call_sid] += "XXX" + i.to_s
+    participant[:conference_sid] += "XXX" + i.to_s
+    list << participant
+  end
+  list
 end
+  
+  
+  
