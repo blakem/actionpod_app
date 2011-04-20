5d26a60db90c201c01a8e6c6575afb2499b46c12
Blake Mills <blakem30@yahoo.com>
2011-04-20 00:13:51 -0700

    minor change for live test

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -141,6 +141,7 @@ class TwilioController < ApplicationController
     def pick_users_closest_event(user)
       user_time = Time.now.in_time_zone(user.time_zone)
       beg_of_day = user_time.beginning_of_day
+      puts user.inspect
       events = user.events.sort { |a,b| a.schedule.next_occurrence(beg_of_day) <=> b.schedule.next_occurrence(beg_of_day) }
       closest_event = events[0]
       events.each do |event|
