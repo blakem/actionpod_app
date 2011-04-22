09d5278f2ea125bc0929f98c582a0e05aa81942f
Blake Mills <blakem30@yahoo.com>
2011-04-22 14:19:58 -0600

    Move heroko_config information to it's own directory

--- script/save_heroku_configs.sh
+++ script/save_heroku_configs.sh
@@ -1,11 +1,11 @@
-echo "*********** heroku config --app actionpods > config/heroku_config"
-heroku config --app actionpods > config/heroku_config
+echo "*********** heroku config --app actionpods > config/heroku/heroku_config"
+heroku config --app actionpods > config/heroku/heroku_config
 
-echo "*********** heroku info --app actionpods > config/heroku_info"
-heroku info --app actionpods > config/heroku_info
+echo "*********** heroku info --app actionpods > config/heroku/heroku_info"
+heroku info --app actionpods > config/heroku/heroku_info
 
-echo "*********** heroku config --app actionpods-staging > config/heroku_staging_config"
-heroku config --app actionpods-staging > config/heroku_staging_config
+echo "*********** heroku config --app actionpods-staging > config/heroku/heroku_staging_config"
+heroku config --app actionpods-staging > config/heroku/heroku_staging_config
 
-echo "*********** heroku info --app actionpods-staging > config/heroku_staging_info"
-heroku info --app actionpods-staging > config/heroku_staging_info
+echo "*********** heroku info --app actionpods-staging > config/heroku/heroku_staging_info"
+heroku info --app actionpods-staging > config/heroku/heroku_staging_info
