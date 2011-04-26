5119be5b83eb79a4063c228f8b013b9d185f06bf
Blake Mills <blakem30@yahoo.com>
2011-04-26 16:41:43 -0700

    Added gravatar to Conference Notes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,10 +7,9 @@ class UserMailer < ActionMailer::Base
   default :from => "15-Minute Calls <support@15minutecalls.com>"
   
   def conference_email(user, participants)
-    @user = user
     @current_user = user
-    @date = Time.now.strftime('%A, %B %d').sub(/ 0/, '')
     @participants = participants
-    mail(:to => user.email, :subject => "[15mc] Conference Notes for #{@date}")
+    @date = Time.now.strftime('%A, %B %d').sub(/ 0/, '')
+    mail(:to => @current_user.email, :subject => "[15mc] Conference Notes for #{@date}")
   end
 end
