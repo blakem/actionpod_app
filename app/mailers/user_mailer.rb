a7959fcb8d4b857af7ec36ddf8a45c2a99a462b5
Blake Mills <blakem30@yahoo.com>
2011-05-05 23:07:58 -0700

    Fixed date bug in conference_email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,8 @@ class UserMailer < ActionMailer::Base
   def conference_email(user, participants)
     @current_user = user
     @participants = [user] + participants.select{ |p| p.id != user.id }
-    @date = Time.now.strftime('%A, %B %d').sub(/ 0/, '')
+    date = Time.now
+    @date = date.strftime("%A, %B #{date.day.ordinalize}")
     @mailer = true
     mail(:to => @current_user.email, :subject => "[15mc] Conference Notes for #{@date}")
   end
