fe8ed9de10232d3d071e2d29886b148bd1e51e79
Blake Mills <blakem30@yahoo.com>
2011-06-03 19:56:42 -0700

    Don't allow users to edit each others pools

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -3,12 +3,13 @@ class PoolsController < ApplicationController
   
   # GET /pools
   def index
-    @pools = Pool.all
+    @pools = current_user.pools
   end
 
   # GET /pools/1
   def show
-    @pool = Pool.find(params[:id])
+    @pool = pool_from_params
+    redirect_to(root_path, :alert => "You don't have permissions to view that group.") unless @pool
   end
 
   # GET /pools/new
@@ -18,14 +19,15 @@ class PoolsController < ApplicationController
 
   # GET /pools/1/edit
   def edit
-    @pool = Pool.find(params[:id])
+    @pool = pool_from_params
+    redirect_to(root_path, :alert => "You don't have permissions to view that group.") unless @pool
   end
 
   # POST /pools
   def create
-    @pool = Pool.new(params[:pool])
+    @pool = Pool.new(params[:pool].merge({:admin_id => current_user.id}))
     if @pool.save
-      redirect_to(@pool, :notice => 'Pool was successfully created.')
+      redirect_to(@pool, :notice => 'Group was successfully created.')
     else
       render :action => "new"
     end
@@ -33,18 +35,31 @@ class PoolsController < ApplicationController
 
   # PUT /pools/1
   def update
-    @pool = Pool.find(params[:id])
-    if @pool.update_attributes(params[:pool])
-      redirect_to(@pool, :notice => 'Pool was successfully updated.')
+    @pool = pool_from_params
+    if @pool
+      if @pool.update_attributes(params[:pool])
+        redirect_to(@pool, :notice => 'Group was successfully updated.')
+      else
+        render :action => "edit"
+      end
     else
-      render :action => "edit"
+      redirect_to(root_path, :alert => "You don't have permissions to view that group.")
     end
   end
 
   # DELETE /pools/1
   def destroy
-    @pool = Pool.find(params[:id])
-    @pool.destroy
-    redirect_to(pools_url)
+    @pool = pool_from_params
+    if @pool
+      @pool.destroy
+      redirect_to(pools_url)
+    else
+      redirect_to(root_path, :alert => "You don't have permissions to view that group.")
+    end
   end
+  
+  private
+    def pool_from_params
+      @current_user.pools.select{ |p| p.id == params[:id].to_i }.first
+    end
 end
--- spec/controllers/pools_controller_spec.rb
+++ spec/controllers/pools_controller_spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 
 describe PoolsController do
 
-  def valid_attributes
-    @user = Factory(:user)
-    {:admin_id => @user.id}
-  end
-
   describe "when not logged in" do
     it "should redirect to the home page" do
   	  controller.user_signed_in?.should be_false
@@ -17,22 +12,39 @@ describe PoolsController do
 
   describe "when logged in" do
     before(:each) do
-      User.all.each { |u| u.destroy }
       login_user
+      @other_user = Factory(:user)
+      @attr = {
+        :admin_id => @other_user.id
+      }
+      @pool1 = Pool.create! @attr
+      @pool2 = Pool.create! @attr
+      @pool_other = Pool.create! @attr
+      @current_user.pools = [@pool1, @pool2]
     end
+    
     describe "GET index" do
-      it "assigns all pools as @pools" do
-        pool = Pool.create! valid_attributes
+      it "assigns all users pools as @pools" do
         get :index
-        assigns(:pools).should eq(Pool.all)
+        assigns(:pools).should     include(@pool1, @pool2)
+        assigns(:pools).should_not include(@pool_other)
       end
     end
 
     describe "GET show" do
       it "assigns the requested pool as @pool" do
-        pool = Pool.create! valid_attributes
-        get :show, :id => pool.id.to_s
-        assigns(:pool).should eq(pool)
+        get :show, :id => @pool1.id.to_s
+        assigns(:pool).should eq(@pool1)
+      end
+      
+      it "redirects if you try to show a pool you don't belong to" do
+        get :show, :id => @pool_other.id
+        response.should redirect_to(root_path)
+      end
+
+      it "redirects if you try to send an arbitrary id" do
+        get :show, :id => 77345
+        response.should redirect_to(root_path)
       end
     end
 
@@ -45,9 +57,18 @@ describe PoolsController do
 
     describe "GET edit" do
       it "assigns the requested pool as @pool" do
-        pool = Pool.create! valid_attributes
-        get :edit, :id => pool.id.to_s
-        assigns(:pool).should eq(pool)
+        get :edit, :id => @pool1.id.to_s
+        assigns(:pool).should eq(@pool1)
+      end
+      
+      it "redirects if you try to show a pool you don't belong to" do
+        get :edit, :id => @pool_other.id
+        response.should redirect_to(root_path)
+      end
+
+      it "redirects if you try to send an arbitrary id" do
+        get :edit, :id => 77345
+        response.should redirect_to(root_path)
       end
     end
 
@@ -55,18 +76,19 @@ describe PoolsController do
       describe "with valid params" do
         it "creates a new Pool" do
           expect {
-            post :create, :pool => valid_attributes
+            post :create, :pool => @attr
           }.to change(Pool, :count).by(1)
         end
 
         it "assigns a newly created pool as @pool" do
-          post :create, :pool => valid_attributes
+          post :create, :pool => @attr
           assigns(:pool).should be_a(Pool)
           assigns(:pool).should be_persisted
+          assigns(:pool).admin_id.should == @current_user.id
         end
 
         it "redirects to the created pool" do
-          post :create, :pool => valid_attributes
+          post :create, :pool => @attr
           response.should redirect_to(Pool.last)
         end
       end
@@ -91,42 +113,41 @@ describe PoolsController do
     describe "PUT update" do
       describe "with valid params" do
         it "updates the requested pool" do
-          pool = Pool.create! valid_attributes
-          # Assuming there are no other pools in the database, this
-          # specifies that the Pool created on the previous line
-          # receives the :update_attributes message with whatever params are
-          # submitted in the request.
           Pool.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-          put :update, :id => pool.id, :pool => {'these' => 'params'}
+          put :update, :id => @pool1.id, :pool => {'these' => 'params'}
         end
 
         it "assigns the requested pool as @pool" do
-          pool = Pool.create! valid_attributes
-          put :update, :id => pool.id, :pool => valid_attributes
-          assigns(:pool).should eq(pool)
+          put :update, :id => @pool1.id, :pool => @attr
+          assigns(:pool).should eq(@pool1)
         end
 
         it "redirects to the pool" do
-          pool = Pool.create! valid_attributes
-          put :update, :id => pool.id, :pool => valid_attributes
-          response.should redirect_to(pool)
+          put :update, :id => @pool1.id, :pool => @attr
+          response.should redirect_to(@pool1)
+        end
+        
+        it "redirects if you try to show someone else's event" do
+          get :update, :id => @pool_other.id
+          response.should redirect_to(root_path)
+        end
+
+        it "redirects if you try to send an arbitrary id" do
+          get :update, :id => 77345
+          response.should redirect_to(root_path)
         end
       end
 
       describe "with invalid params" do
         it "assigns the pool as @pool" do
-          pool = Pool.create! valid_attributes
-          # Trigger the behavior that occurs when invalid params are submitted
           Pool.any_instance.stub(:save).and_return(false)
-          put :update, :id => pool.id.to_s, :pool => {}
-          assigns(:pool).should eq(pool)
+          put :update, :id => @pool1.id.to_s, :pool => {}
+          assigns(:pool).should eq(@pool1)
         end
 
         it "re-renders the 'edit' template" do
-          pool = Pool.create! valid_attributes
-          # Trigger the behavior that occurs when invalid params are submitted
           Pool.any_instance.stub(:save).and_return(false)
-          put :update, :id => pool.id.to_s, :pool => {}
+          put :update, :id => @pool1.id.to_s, :pool => {}
           response.should render_template("edit")
         end
       end
@@ -134,16 +155,17 @@ describe PoolsController do
 
     describe "DELETE destroy" do
       it "destroys the requested pool" do
-        pool = Pool.create! valid_attributes
         expect {
-          delete :destroy, :id => pool.id.to_s
+          delete :destroy, :id => @pool1.id.to_s
         }.to change(Pool, :count).by(-1)
+        response.should redirect_to(pools_url)
       end
 
-      it "redirects to the pools list" do
-        pool = Pool.create! valid_attributes
-        delete :destroy, :id => pool.id.to_s
-        response.should redirect_to(pools_url)
+      it "Doesn't allow you to delete someone elses events" do
+        other_pool_id = @pool_other.id
+        delete :destroy, :id => other_pool_id
+        Pool.find_by_id(other_pool_id).should_not be_nil
+        response.should redirect_to(root_path)
       end
     end
   end
